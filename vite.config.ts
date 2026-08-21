719d232e2cd93d843d99b75b7861cc078320c508
Bruno Alberto <bncreativedigital@gmail.com>
2026-08-20 23:49:31 -0300

    Configure project for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+base: '/AKITEM-Controle-de-validade/',
+
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
